52a04c92ad1a54e9b69581a9602ada4f6f952fa3
SAROJ-2207 <sksaroj828@gmail.com>
2022-10-04 09:31:23 +0530

    ADD GENDER AND DEPARTMENT

--- Uc 12 ADD GENDER AND DEPARTMENT/service/IEmployeePayrollService.java
+++ Uc 12 ADD GENDER AND DEPARTMENT/service/IEmployeePayrollService.java
@@ -11,6 +11,7 @@ public interface IEmployeePayrollService {
     EmployeePayrollDataModel updateEmployee(long id, EmployeePayrollDataModel EmployeePayrollDataModel);
     EmployeePayrollDataModel deleteEmployeeById(long id);
     Optional<EmployeePayrollDataModel> getEmployeeDataById(long empId);
+//    public List<EmployeePayrollDataModel> findDataByName(String empName);
     public List<EmployeePayrollDataModel> findDataByDepartment(String department);
 
 }
